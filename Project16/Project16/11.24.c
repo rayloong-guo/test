5984649910a74774a962c6b73900ab2eb450a73d
rayloong-guo <ruilong0824@163.com>
2020-11-25 12:47:15 +0800

    im insane

---
+++ Project16/Project16/11.24.c
@@ -0,0 +1,42 @@
+#define  _CRT_SECURE_NO_WARNINGS  1
+//√∞≈›≈≈–Ú  bubble_sort
+#include<stdio.h>
+//void bubble_sort(int arr[],int sz)
+//{
+//	int  i = 0;
+//	for (i = 0; i < 9; i++)
+//	{
+//		int j = 0;
+//		int flag = 1;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j]>arr[j + 1])
+//			{
+//				int tem = arr[j];
+//				arr[j ] = arr[j+1];
+//				arr[j+1] = tem;
+//				flag = 0;
+//			}
+//		}
+//		if (flag == 1)
+//		{
+//			break;
+//		}
+//	}
+//}
+//
+//int main()
+//{
+//	int arr[] = { 1,5,7,20,31,40,62,4,6,85 };
+//	printf("%p\n", &arr);
+//	printf("%d\n", &arr);
+//	int i = 0;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	bubble_sort(arr, sz);
+//	for (i = 0; i < 10; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
+
